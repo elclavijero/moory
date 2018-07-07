caf8a5dafa16d0e0b419b1609b9110de57a64010
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-07 19:06:01 +0100

    ab_star example

---
+++ examples/ab_star.rb
@@ -0,0 +1,20 @@
+require 'moory'
+
+ab_star = Moory::Recogniser.create(
+  initial: '0',
+  transitions: %q{
+    0 : a : 1
+    0 : b : 2
+    1 : a : 2
+    1 : b : 1
+    2 : a : 2
+    2 : b : 2
+  },
+  final: %w{ 1 }
+)
+
+
+ab_star.accepts?(string: "abb")      
+# => true
+ab_star.accepts?(string:"abb", in_state: '1')
+# => false
