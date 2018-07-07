ad8d8ff6fd7ce832d53aa8ecdb034f26722220e9
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-07 19:05:28 +0100

    accepts? in_state:

--- lib/moory/recogniser.rb
+++ lib/moory/recogniser.rb
@@ -4,8 +4,8 @@ module Moory
       Moory::Interpreter.new do |i|
         i.load(specification[:transitions])
       
-        define_singleton_method(:accepts?) do |string, state=nil|
-          i.state = state ? state : specification[:initial]
+        define_singleton_method(:accepts?) do |string:, in_state: nil|
+          i.state = in_state ? in_state : specification[:initial]
           
           string.each_char { |c| putm(c) }
       
