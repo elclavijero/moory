3f8c41567c1548e6be0d73995dfbcfef4e009b2b
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-08 11:16:01 +0100

    Forgot to define an error handler on the Aceptor.  Ooops!

--- lib/moory/acceptor.rb
+++ lib/moory/acceptor.rb
@@ -3,6 +3,10 @@ module Moory
     def Acceptor.create(specification)
       Moory::Interpreter.new do |i|
         i.load(specification[:transitions])
+        
+        i.default_proc = proc { |msg| 
+          raise "#{i} in state #{i.state} did not understand #{msg}.  Input rejected!"
+        }
       
         define_singleton_method(:accepts?) do |string:, in_state: nil|
           i.state = in_state ? in_state : specification[:initial]
