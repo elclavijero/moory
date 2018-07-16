99524dc47fcb5bacb371cf6c83312bfb3aba85a5
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-16 13:00:02 +0100

    Before I break something

--- lib/moory/transitions.rb
+++ lib/moory/transitions.rb
@@ -1,4 +1,67 @@
 module Moory
-  class Transitions
+  module Transition
+    class Storage
+      def storage
+        @storage ||= {}
+      end
+  
+      def count
+        storage.size
+      end
+  
+      def store(params)
+        storage.merge!(transition(params))
+      end
+  
+      private
+  
+      def transition(params)
+        poise(params).shunt(response(params))
+      end
+  
+      def poise(params)
+        Moory::Pair.new(
+          left:  params['source'],
+          right: params['stimulus']
+        )
+      end
+  
+      def response(params)
+        { 
+          state:    params['target'],
+          output:   params['output'],
+          effector: params['effector']
+        }.compact
+      end
+    end
+
+    Record = Struct.new(
+      :origin,
+      :stimulus,
+      :settlement,
+      :output,
+      :effector, 
+      keyword_init: true
+    ) do
+
+      def valid?
+        origin && stimulus && settlement
+      end
+
+      def poise
+        Moory::Pair.new(
+          left:  origin,
+          right: stimulus
+        )
+      end
+
+      def response
+        { 
+          settlement: settlement,
+          output:     output,
+          effector:   effector
+        }.compact
+      end
+    end
   end
 end
--- spec/transitions_spec.rb
+++ spec/transitions_spec.rb
@@ -1,2 +1,19 @@
-RSpec.describe Moory::Transitions do
+RSpec.describe Moory::Transition::Storage do
+  let(:transitions) do
+    Moory::Transition::Storage.new
+  end
+
+  describe '#store' do
+    context 'given parameters including at least origin, stimulus, and settlement' do
+      it 'will increase #count by 1' do
+        expect{
+          transitions.store(origin: '0', stimulus: 'a', settlement: '1')
+        }.to change{
+          transitions.count
+        }.by(
+          1
+        )
+      end
+    end
+  end
 end
