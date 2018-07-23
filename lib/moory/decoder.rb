a50c886c0f21ffee53ecfd4c634b2a638c3abda2
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-23 17:54:05 +0100

    YARD for Decoder

--- lib/moory/decoder.rb
+++ lib/moory/decoder.rb
@@ -9,6 +9,17 @@ module Moory
       configure(rules)
     end
 
+    # Decode a string according to the rules.  Writes the decoded string to the output 
+    #   stream configured at initialisation (which is $stdout, by default).  Characters
+    #   not belonging to the alphabet will be dropped.
+    #
+    # @param string [String] the string you wish to decode
+    def decode(string)
+      string.each_char { |c| issue(c) }
+    end
+
+    private
+
     def configure(rules)
       Loader.load(rules: rules, machine: self)
       repertoire.always = method(:write)
@@ -16,10 +27,6 @@ module Moory
 
     def write(output=nil)
       @ostream.write(output) if output
-    end
-
-    def decode(string)
-      string.each_char { |c| issue(c) }
     end
   end
 end
