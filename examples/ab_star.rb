5d87f33e110b507789aadd7fefaef923b250d7b5
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-23 13:41:46 +0100

    ab_star

--- examples/ab_star.rb
+++ examples/ab_star.rb
@@ -1,33 +1,7 @@
 require 'moory'
 
-module Moory
-  class Recogniser < Machine
-    def initialize(rules:, initial:, final:)
-      super()
-      @initial = initial
-      @final   = final
-      Loader.load(rules: rules, machine: self)
-    end
-
-    def accepts?(string)
-      reset
-
-      string.each_char.all? { |c| issue(c) } && accepting?
-    end
-
-    private
-
-    def reset
-      @state = @initial
-    end
-
-    def accepting?
-      @final.include?(state)
-    end
-  end
-end
-
 rcg = Moory::Recogniser.new(
+  initial: '0',
   rules: %q{
     0 : a : 1
     0 : b : 2
@@ -36,16 +10,15 @@ rcg = Moory::Recogniser.new(
     2 : a : 2
     2 : b : 2
   },
-  initial: '0',
   final:  [
     '1'
   ]
 )
 
-pp rcg.accepts?("a") == true
-pp rcg.accepts?("b") == false
-pp rcg.accepts?("c") == false
-pp rcg.accepts?("ab") == true
-pp rcg.accepts?("ac") == false
-pp rcg.accepts?("abb") == true
-pp rcg.accepts?("abc") == false
+rcg.accepts?("a")   # => true
+rcg.accepts?("b")   # => false
+rcg.accepts?("c")   # => false
+rcg.accepts?("ab")  # => true
+rcg.accepts?("ac")  # => false
+rcg.accepts?("abb") # => true
+rcg.accepts?("abc") # => false
