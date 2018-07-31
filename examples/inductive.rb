2b9b58d103a567f2849fe0cd61adaa9f77188df7
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-31 12:13:47 +0100

    Prefix/Infix/Parenthetical/Constant (PIPC) recogniser

--- examples/inductive.rb
+++ examples/inductive.rb
@@ -1,46 +1,77 @@
 require 'moory'
 
-prefix_infix_constant_config = {
+prefix_infix_parenthetical_constant_config = {
   basis: 'basis',
   specs: {
     'basis' => {
       rules: """
       ^ : constant : $
 
-      ^ : prefix / open / defer : Δ
+      ^ : prefix / open          / defer : Δ
 
-      $ : infix  / open / defer : Δ
+      ^ : (      / parenthetical / defer : Δ
+
+      $ : infix  / open          / defer : Δ
       
       Δ : term : $
       """,
     },
     'open' => {
       rules: """
-      ^ : constant / term / reconvene : $
+      ^ : constant / term          / reconvene : $
 
-      ^ : prefix / open / defer : Δ
+      ^ : prefix   / open          / defer : Δ
 
-      $ : infix  / open / defer : Δ
+      ^ : (        / parenthetical / defer : Δ
+
+      $ : infix    / open          / defer : Δ
       
-      Δ : term / term / reconvene : $
+      Δ : term     / term          / reconvene : $
       """,
+    },
+    'parenthetical' => {
+      rules: """
+      ^ : constant : C
+
+      ^ : prefix / open          / defer : Δ
+
+      ^ : (      / parenthetical / defer : Δ
+
+      ^ : )      / void          / reconvene : $
+
+      C : infix  / open          / defer : Δ
+
+      C : )      / term          / reconvene : $
+      
+      Δ : term : C
+      """
     }
   }
 }
 
-logistic = Moory::Logistic::Controller.new(prefix_infix_constant_config)
+logistic = Moory::Logistic::Controller.new(prefix_infix_parenthetical_constant_config)
 
 %w{
-  constant
-  infix
-  prefix
-  constant
-  infix
-  constant
-  infix
-  prefix
+  (
+    (
+      prefix
+      constant
+      infix
+      prefix
+      (
+        constant
+        infix
+        prefix
+        (
+          constant
+        )
+      )
+    )
+  ) 
 }.each do |w|
   logistic.issue(w)
 end
 
 pp logistic.done?
+
+pp logistic.deferrals
