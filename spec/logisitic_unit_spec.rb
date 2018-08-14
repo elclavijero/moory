de469cb28740733e4f1f6c1b02f8e7cc41b2ac67
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-14 20:40:08 +0100

    OK

--- spec/logisitic_unit_spec.rb
+++ spec/logisitic_unit_spec.rb
@@ -8,16 +8,36 @@ RSpec.describe Moory::Logistic::Unit do
   end
 
   describe '#issue' do
-    context 'providing a quarantine proc has not been defined' do
-      context 'given a foreign stimulus' do
-        let(:foreign_stimulus) do
-          'blah'
+    context 'given a foreign stimulus,' do
+      let(:foreign_stimulus) do
+        'blah'
+      end
+
+      context 'providing a quarantine object has NOT been defined,' do
+        before do
+          the_unit.quarantine = nil
         end
 
-        it 'will raise an exception' do
+        it 'the Unit will raise an exception' do
           expect{
             the_unit.issue(foreign_stimulus)
           }.to raise_error("Unexpected #{foreign_stimulus}")
+        end
+      end
+
+      context 'providing a quarantine object has been defined' do
+        before do
+          the_unit.quarantine = the_quarantine
+        end
+
+        let(:the_quarantine) do
+          spy('the qurantine')
+        end
+
+        it 'the Unit will call the quarantine with the foreign stimulus' do
+          the_unit.issue(foreign_stimulus)
+
+          expect(the_quarantine).to have_received(:call).with(foreign_stimulus)
         end
       end
     end
